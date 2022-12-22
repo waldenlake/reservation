7d397b8c952cd44587787b8c59b61062bb5b3503
walden <786358869@qq.com>
2022-12-22 10:15:03 +0800

    update pre-commit setting

--- target/debug/deps/reservation_service-9c5bf47a60e6b622.d
+++
@@ -1,5 +0,0 @@
-/root/space/rust/reservation/target/debug/deps/reservation_service-9c5bf47a60e6b622: service/src/main.rs
-
-/root/space/rust/reservation/target/debug/deps/reservation_service-9c5bf47a60e6b622.d: service/src/main.rs
-
-service/src/main.rs:
